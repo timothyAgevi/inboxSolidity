e6e7ba41664b9433509cc2afe67b7a0a657795a2
timothyAgevi <timothyagevi@gmail.com>
2022-02-27 22:47:08 +0300

    update deployment

--- deploy.js
+++ deploy.js
@@ -1,12 +1,13 @@
- const HDWalletProvider= require('truffle-wallet-provider');
+const HDWalletProvider= require('truffle-wallet-provider');
 const Web3= require('web3');
-const{interface,bytecode}=require('./compile')
+const{bytecode, abi}=require('./compile')
 
 const provider = new HDWalletProvider(
     'virtual first dawn mail wire helmet buffalo print pencil where allow scatter',
     // url of network to connect to
     'https://rinkeby.infura.io/v3/2d040a93bf9143c0815bd73f1f76ac85'
 )
+
 const web3=new Web3(provider); 
 
 const deploy = async ()=>{
@@ -14,7 +15,7 @@ const deploy = async ()=>{
     const accounts = await web3.eth.getAccounts();
     //
     console.log('Attempting to deploy from account',accounts[0])
-  const result =  await new web3.eth.Contract(JSON.parse(interface))
+  const result =  await new web3.eth.Contract(JSON.parse(abi))
     .deploy({ data:bytecode,arguments:['Hi there!']})
     .send({ gas: '1000000', from:accounts[0]});
 
